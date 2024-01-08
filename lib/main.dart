3edce0cd067d31e08f46d39bcc4005f323d0b0d9
vivekg007 <vivek0077psg@outlook.com>
2024-01-08 11:20:23 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Amazon());
+}
+
+class Amazon extends StatelessWidget {
+  const Amazon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
